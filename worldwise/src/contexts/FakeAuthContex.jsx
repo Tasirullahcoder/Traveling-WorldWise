6ae1b642de65a068b5585fb49071111b28a58e8e
Tasir Ullah <https://github.com/Tasirullahcoder/WDD18>
2025-12-16 14:49:26 +0500

    Add FakeAuthenticationContext

---
+++ worldwise/src/contexts/FakeAuthContex.jsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useReducer } from "react";
+
+const AuthContext = createContext();
+const FAKE_USER = {
+  name: "Jack",
+  emaiL: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+const initialstate = {
+  user: null,
+  isAuthenticated: false,
+};
+function Reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+  }
+}
+function FakeAuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    Reducer,
+    initialstate
+  );
+  function login({ email, password }) {
+    if (email === FAKE_USER.emaiL && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+  function logout() {
+    dispatch({ type: "logout", payload: FAKE_USER });
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+function UseAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("UseAuth must be used within a AuthProvider");
+  }
+}
+
+export { FakeAuthProvider, UseAuth };
